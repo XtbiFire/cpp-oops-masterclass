8d3903b15b2f174f3a888a5f0cfcb6e6f321cc17
XtbiFire <xtbifire@gmail.com>
2026-07-11 11:00:14 +0530

    Create 01_What_Is_OOP.cpp

---
+++ 01_What_Is_OOP.cpp
@@ -0,0 +1,78 @@
+/*
+◆━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◆ 01. What Is OOP
+◆━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◆
+
+💡 Remember
+
+Object-Oriented Programming (OOP) is a programming
+paradigm that groups Data and Functions into Objects.
+
+✔ Organizes Large Software
+✔ Code Reusability
+✔ Better Security
+✔ Easy Maintenance
+✔ Team Collaboration
+
+Real Life Examples:
+• Car
+• Mobile
+• Laptop
+• Bank Account
+• Player
+• Enemy
+
+Four Pillars:
+1. Encapsulation
+2. Inheritance
+3. Polymorphism
+4. Abstraction
+
+Player Object
+
+Data
+- Name
+- Health
+- Level
+- Score
+
+Functions
+- Run()
+- Jump()
+- Attack()
+- Reload()
+
+🎯 Interview Definition
+
+Object-Oriented Programming is a programming
+paradigm that groups data and functions into
+objects to build modular, reusable,
+maintainable and secure software.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Object-Oriented Programming (OOP)" << endl;
+    cout << "Programming Paradigm" << endl;
+    cout << "Data + Functions = Objects" << endl;
+    cout << "Large Software Organization" << endl;
+    cout << "Reusability" << endl;
+    cout << "Security" << endl;
+    cout << "Maintainability" << endl;
+
+    return 0;
+}
+
+/*
+Output:
+Object-Oriented Programming (OOP)
+Programming Paradigm
+Data + Functions = Objects
+Large Software Organization
+Reusability
+Security
+Maintainability
+*/
